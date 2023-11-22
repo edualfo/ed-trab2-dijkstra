212d98827fb6756174e21dd09fc25a9452668daa
eduardoabreulfo <eduardo.abreulfo@gmail.com>
2023-11-22 14:10:40 -0300

    heap operations added in the header

--- headers/heap.h
+++ headers/heap.h
@@ -1,2 +1,16 @@
 
+typedef unsigned char bool;
 typedef struct Heap Heap;
+
+
+Heap *heap_construct(); // O(1)
+void heap_push(Heap *heap, void *data,
+  double priority); // O(log N)
+bool heap_empty(Heap *heap); // O(1)
+void *heap_max(Heap *heap); // O(1)
+double heap_max_priority(Heap *heap); // O(1)
+void *heap_pop(Heap *heap); // O(log N)
+void heap_destroy(Heap *heap); // O(1)
+void heap_sort(
+  void *array, int array_size, int item_size,
+  double (*key_fn)(void *)); // O(N log N)
